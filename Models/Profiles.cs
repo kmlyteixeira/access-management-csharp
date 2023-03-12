ec991d9b5bae689b2584fa0bbe812f06af12bcbe
kemily <kemilyteixeira047@gmail.com>
2023-03-12 19:36:42 -0300

    feat: update models - create methods CRUD

--- Models/Profiles.cs
+++ Models/Profiles.cs
@@ -1,24 +1,85 @@
+using Repository;
+using Utils;
+
 namespace Models
 {
-  public enum ProfileType
-  {
-    Admin = 1,
-    User = 2
-  }
-
   public class Profiles
   {
     public int Id { get; set; }
     public int UserId { get; set; }
     public virtual Users User { get; set; }
-    public ProfileType ProfileType { get; set; }
+    public Enumerators.ProfileType ProfileType { get; set; }
 
     public Profiles() { }
     
-    public Profiles(Users user, ProfileType profileType)
+    public Profiles(Users user, Enumerators.ProfileType profileType)
     {
       this.UserId = user.Id;
       this.ProfileType = profileType;
+
+      Context db = new Context();
+      db.Profiles.Add(this);
+      db.SaveChanges();
+    }
+
+    public static Profiles GetProfileById(int id)
+    {
+      Context db = new Context();
+
+      return db.Profiles.Find(id);
+    }
+
+    public static Profiles GetProfileByUser(Users user)
+    {
+      Context db = new Context();
+
+      return (from profile in db.Profiles
+              where profile.UserId == user.Id
+              select profile).First();
+    }
+
+    public static int CountAdminProfiles()
+    {
+      Context db = new Context();
+
+      return (from profile in db.Profiles
+              where profile.ProfileType == Enumerators.ProfileType.Admin
+              select profile).Count();
+    }
+
+    public static int CountUserProfiles()
+    {
+      Context db = new Context();
+
+      return (from profile in db.Profiles
+              where profile.ProfileType == Enumerators.ProfileType.User
+              select profile).Count();
+    }
+
+    public static Profiles UpdateProfile(Profiles profile, Enumerators.ProfileType profileType)
+    {
+      Context db = new Context();
+
+      profile.ProfileType = profileType;
+
+      db.SaveChanges();
+
+      return profile;
+    }
+
+    public static Profiles DeleteProfile(Profiles profile)
+    {
+      Context db = new Context();
+
+      db.Profiles.Remove(profile);
+      db.SaveChanges();
+
+      return profile;
+    }
+
+    public override string ToString()
+    {
+      return this.ProfileType.ToString();
     }
   }
 }
--- Models/Sessions.cs
+++ Models/Sessions.cs
@@ -1,19 +1,59 @@
-namespace Models {
-    public class Sessions {
-        public int Id { get; set; }
-        public virtual Users User { get; set; }
-        public int UserId { get; set; }
-        public string Token { get; set; }
-        public DateTime CreatedDate { get; set; }
-        public DateTime ExpirationDate { get; set; }
+using Repository;
 
-        public Sessions() { }
+namespace Models
+{
+  public class Sessions
+  {
+    public int Id { get; set; }
+    public virtual Users User { get; set; }
+    public int UserId { get; set; }
+    public string Token { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime ExpirationDate { get; set; }
 
-        public Sessions(Users user, string token, DateTime createdDate, DateTime expirationDate) {
-            this.UserId = user.Id;
-            this.Token = token;
-            this.CreatedDate = createdDate;
-            this.ExpirationDate = expirationDate;
-        }
+    public Sessions() { }
+
+    public Sessions(Users user, string token, DateTime createdDate, DateTime expirationDate)
+    {
+      this.UserId = user.Id;
+      this.Token = token;
+      this.CreatedDate = createdDate;
+      this.ExpirationDate = expirationDate;
+
+      Context db = new Context();
+      db.Sessions.Add(this);
+      db.SaveChanges();
     }
+
+    public static Sessions GetSessionById(int id)
+    {
+      Context db = new Context();
+
+      return db.Sessions.Find(id);
+    }
+
+    public static Sessions DeleteSession(Sessions session)
+    {
+      Context db = new Context();
+
+      db.Sessions.Remove(session);
+      db.SaveChanges();
+
+      return session;
+    }
+
+    public static IEnumerable<Sessions> GetAllSessions()
+    {
+      Context db = new Context();
+
+      return from session in db.Sessions
+             select session;
+    }
+
+    public override string ToString()
+    {
+      return $"Id: {this.Id} - Token: {this.Token} - CreatedDate: {this.CreatedDate} - ExpirationDate: {this.ExpirationDate} - User: {Controllers.Users.GetUserById(this.UserId.ToString())}";
+    }
+
+  }
 }
