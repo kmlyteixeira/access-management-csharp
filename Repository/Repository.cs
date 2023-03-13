494927655e8add09be63210d44bd89b2d789e085
kemily <kemilyteixeira047@gmail.com>
2023-03-13 16:43:15 -0300

    style: code indentation

--- Repository/Repository.cs
+++ Repository/Repository.cs
@@ -1,13 +1,16 @@
 using Microsoft.EntityFrameworkCore;
 
-namespace Repository {
-    public class Context : DbContext {
-        public DbSet<Models.Users> Users { get; set; }
-        public DbSet<Models.Profiles> Profiles { get; set; }
-        public DbSet<Models.Sessions> Sessions { get; set; }
+namespace Repository
+{
+  public class Context : DbContext
+  {
+    public DbSet<Models.Users> Users { get; set; }
+    public DbSet<Models.Profiles> Profiles { get; set; }
+    public DbSet<Models.Sessions> Sessions { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
-            optionsBuilder.UseMySql("Server=localhost;User Id=root;Database=accessmanage");
-        }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseMySql("Server=localhost;User Id=root;Database=accessmanage");
     }
+  }
 }
